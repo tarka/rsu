a787650970be384cfe60806d194ecb186b827b5a
Steve Smith <tarkasteve@gmail.com>
2022-11-05 13:24:53 +1100

    Update dependencies.

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,7 +4,7 @@ use docker_test as dt;
 
 #[test]
 fn help() {
-    let container = dt::setup("usu", None).unwrap();
+    let container = dt::setup("usu", None, None, "1.64.0").unwrap();
     let out = container.exec(vec![container.dest_str(), "--help"]).unwrap();
     let stdout = String::from_utf8(out.stdout).unwrap();
     assert!(out.status.success());
@@ -13,7 +13,7 @@ fn help() {
 
 #[test]
 fn not_root() {
-    let container = dt::setup("usu", None).unwrap();
+    let container = dt::setup("usu", None, None, "1.64.0").unwrap();
     let out = container.exec_as(dt::TESTUSER, vec![container.dest_str(), "/bin/ls"]).unwrap();
     assert!(!out.status.success());
     assert!(String::from_utf8(out.stderr).unwrap()

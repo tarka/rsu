814a3b10e516a74d4d77516e06ecff327939ed8e
Steve Smith <tarkasteve@gmail.com>
2022-11-05 14:10:11 +1100

    Bump rust test version

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,10 +1,11 @@
 
 use docker_test as dt;
 
+const RUST_VERSION: &str = "1.65.0";
 
 #[test]
 fn help() {
-    let container = dt::setup("usu", None, None, "1.64.0").unwrap();
+    let container = dt::setup("usu", None, None, RUST_VERSION).unwrap();
     let out = container.exec(vec![container.dest_str(), "--help"]).unwrap();
     let stdout = String::from_utf8(out.stdout).unwrap();
     assert!(out.status.success());
@@ -13,7 +14,7 @@ fn help() {
 
 #[test]
 fn not_root() {
-    let container = dt::setup("usu", None, None, "1.64.0").unwrap();
+    let container = dt::setup("usu", None, None, RUST_VERSION).unwrap();
     let out = container.exec_as(dt::TESTUSER, vec![container.dest_str(), "/bin/ls"]).unwrap();
     assert!(!out.status.success());
     assert!(String::from_utf8(out.stderr).unwrap()

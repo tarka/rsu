36536b0fe00a0cbca2837998918428d31b36df46
Steve Smith <tarkasteve@gmail.com>
2023-04-30 14:28:56 +1000

    Update to latest docker-test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,12 +1,13 @@
 
-use docker_test as dt;
+use docker_test::util::build_and_deploy;
 
 const RUST_VERSION: &str = "1.65.0";
+const TEST_USER: &str = "nobody";
 
 #[test]
 fn help() {
-    let container = dt::setup("usu", None, None, RUST_VERSION).unwrap();
-    let out = container.exec(vec![container.dest_str(), "--help"]).unwrap();
+    let (container, bin) = build_and_deploy("usu", None, None, RUST_VERSION).unwrap();
+    let out = container.exec(vec![bin.as_str(), "--help"]).unwrap();
     let stdout = String::from_utf8(out.stdout).unwrap();
     assert!(out.status.success());
     assert!(stdout.contains("Run commands as a user"));
@@ -14,8 +15,8 @@ fn help() {
 
 #[test]
 fn not_root() {
-    let container = dt::setup("usu", None, None, RUST_VERSION).unwrap();
-    let out = container.exec_as(dt::TESTUSER, vec![container.dest_str(), "/bin/ls"]).unwrap();
+    let (container, bin) = build_and_deploy("usu", None, None, RUST_VERSION).unwrap();
+    let out = container.exec_as(TEST_USER, vec![bin.as_str(), "/bin/ls"]).unwrap();
     assert!(!out.status.success());
     assert!(String::from_utf8(out.stderr).unwrap()
             .contains("Error: Not running as root"));
